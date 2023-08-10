ca75b040f97db168ed76b093eca27824ff5b1402
강승우 <112797234+phonil@users.noreply.github.com>
2023-08-10 22:57:55 +0900

    feat: 3가지 랜덤 음식 목록 조회 api 구현 (#22)
    Co-authored-by: Sejin Park <95167215+sejineer@users.noreply.github.com>

--- src/main/java/com/beginvegan/domain/food/application/FoodService.java
+++ src/main/java/com/beginvegan/domain/food/application/FoodService.java
@@ -1,19 +1,13 @@
 package com.beginvegan.domain.food.application;
 
-import com.beginvegan.domain.block.dto.BlockDto;
 import com.beginvegan.domain.food.domain.Food;
 import com.beginvegan.domain.food.domain.repository.FoodRepository;
-import com.beginvegan.domain.food.dto.FoodIngredientDto;
-import com.beginvegan.domain.food.dto.request.FoodDetailReq;
-import com.beginvegan.domain.food.dto.response.FoodDetailRes;
-import com.beginvegan.domain.food.exception.FoodNotFoundException;
+import com.beginvegan.domain.food.dto.response.FoodListRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+
+import java.util.*;
 
 @RequiredArgsConstructor
 @Service
@@ -21,37 +15,30 @@ public class FoodService {
 
     private final FoodRepository foodRepository;
 
-    // food_id를 통한 레시피 검색
-    @Transactional
-    public ResponseEntity<?> findFoodDetail(FoodDetailReq foodDetailReq) {
-        Optional<Food> foodOptional = foodRepository.findById(foodDetailReq.getId());
-        Food food = foodOptional.orElseThrow(() -> new FoodNotFoundException("해당 아이디를 가진 음식을 찾을 수 없습니다. ID: " + foodDetailReq.getId()));
+    // 3가지 음식 랜덤 조회 : 메인 페이지
+    public ResponseEntity<?> findThreeFoods() {
+        List<Food> foods = foodRepository.findAll();
+        List<FoodListRes> foodList = new ArrayList<>();
 
-        List<FoodIngredientDto> ingredientDtos = food.getIngredients().stream()
-                .map(ingredient -> FoodIngredientDto.builder()
-                        .id(ingredient.getId())
-                        .name(ingredient.getName())
-                        .build())
-                .collect(Collectors.toList());
+        // 랜덤 수 3개 추리기
+        Set<Integer> randomNum = new HashSet<>();
+        while(randomNum.size() < 3){
+            randomNum.add((int)(Math.random() * foods.size()));
+        }
 
-        List<BlockDto> blockDtos = food.getFoodBlocks().stream()
-                .map(block -> BlockDto.builder()
-                        .id(block.getId())
-                        .content(block.getContent())
-                        .sequence(block.getSequence())
-                        .imageSource(block.getImageSource())
-                        .build())
-                .collect(Collectors.toList());
+        Iterator<Integer> iter = randomNum.iterator();
+        while(iter.hasNext()){
+            int num = iter.next();
+            FoodListRes foodListRes = FoodListRes.builder()
+                    .id(foods.get(num).getId())
+                    .name(foods.get(num).getName())
+                    .veganType(foods.get(num).getVeganType())
+                    .description(foods.get(num).getDescription())
+                    .build();
+            foodList.add(foodListRes);
+        }
 
-        FoodDetailRes foodDetailRes = FoodDetailRes.builder()
-                .id(food.getId())
-                .name(food.getName())
-                .veganType(food.getVeganType())
-                .ingredientDtos(ingredientDtos)
-                .blockDtos(blockDtos)
-                .build();
-
-        return ResponseEntity.ok(foodDetailRes);
-
+        return ResponseEntity.ok(foodList);
     }
+
 }
--- src/main/java/com/beginvegan/domain/food/presentation/FoodController.java
+++ src/main/java/com/beginvegan/domain/food/presentation/FoodController.java
@@ -1,8 +1,7 @@
 package com.beginvegan.domain.food.presentation;
 
 import com.beginvegan.domain.food.application.FoodService;
-import com.beginvegan.domain.food.dto.request.FoodDetailReq;
-import com.beginvegan.domain.food.dto.response.FoodDetailRes;
+import com.beginvegan.domain.food.dto.response.FoodListRes;
 import com.beginvegan.global.payload.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -12,8 +11,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,15 +23,15 @@ public class FoodController {
 
     private final FoodService foodService;
 
-    // 레시피 상세 정보 조회
-    @Operation(summary = "레시피 상세 정보 조회", description = "food_id를 통한 레시피 상세 정보를 조회합니다.")
+    // 랜덤 음식 3가지 조회
+    @Operation(summary = "3가지 음식 목록 조회", description = "3가지 음식 목록 조회")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "레시피 상세 정보 조회 성공", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = FoodDetailRes.class)) } ),
-            @ApiResponse(responseCode = "400", description = "레시피 상세 정보 조회 실패", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class) ) } ),
+            @ApiResponse(responseCode = "200", description = "3가지 음식 목록 조회 성공", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = FoodListRes.class)) } ),
+            @ApiResponse(responseCode = "400", description = "3가지 음식 목록 조회 실패", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class) ) } ),
     })
-    @PostMapping("/recipe-detail")
-    public ResponseEntity<?> findFoodDetail(@RequestBody FoodDetailReq foodDetailReq) { // @RequestParam Long id
-        return foodService.findFoodDetail(foodDetailReq);
+    @GetMapping("/random-food-list")
+    public ResponseEntity<?> findThreeFoods(){
+        return foodService.findThreeFoods();
     }
   
 }
