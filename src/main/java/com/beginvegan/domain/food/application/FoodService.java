0a83df1dbe2213d43a72b8aecad2cd2ac8f5839a
phonil <phonil@naver.com>
2023-08-11 09:58:13 +0900

    chore: food 관련 풀리퀘스트 적용, @Transactional 수정

--- src/main/java/com/beginvegan/domain/food/application/FoodService.java
+++ src/main/java/com/beginvegan/domain/food/application/FoodService.java
@@ -1,19 +1,87 @@
 package com.beginvegan.domain.food.application;
 
+import com.beginvegan.domain.block.dto.BlockDto;
 import com.beginvegan.domain.food.domain.Food;
 import com.beginvegan.domain.food.domain.repository.FoodRepository;
+import com.beginvegan.domain.food.dto.FoodIngredientDto;
+import com.beginvegan.domain.food.dto.FoodRecipeListRes;
+import com.beginvegan.domain.food.dto.request.FoodDetailReq;
+import com.beginvegan.domain.food.dto.response.FoodDetailRes;
 import com.beginvegan.domain.food.dto.response.FoodListRes;
+import com.beginvegan.domain.food.exception.FoodNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Service
 public class FoodService {
 
     private final FoodRepository foodRepository;
+
+    // 레시피 전체 조회 : 재료 포함 :: 하단 바 레시피 클릭 시 화면
+    public ResponseEntity<?> findAllFoodsWithIngredients() {
+        List<Food> foods = foodRepository.findAll();
+        List<FoodRecipeListRes> foodDtos = new ArrayList<>();
+
+        for (Food food : foods) {
+            List<FoodIngredientDto> foodIngredientDtos = food.getIngredients().stream()
+                    .map(ingredient -> FoodIngredientDto.builder()
+                            .id(ingredient.getId())
+                            .name(ingredient.getName())
+                            .build())
+                    .collect(Collectors.toList());
+
+            FoodRecipeListRes foodRecipeListRes = FoodRecipeListRes.builder()
+                    .id(food.getId())
+                    .name(food.getName())
+                    .veganType(food.getVeganType())
+                    .ingredients(foodIngredientDtos)
+                    .build();
+
+            foodDtos.add(foodRecipeListRes);
+        }
+
+        return ResponseEntity.ok(foodDtos);
+    }
+
+    // food_id를 통한 레시피 검색
+    public ResponseEntity<?> findFoodDetail(FoodDetailReq foodDetailReq) {
+        Optional<Food> foodOptional = foodRepository.findById(foodDetailReq.getId());
+        Food food = foodOptional.orElseThrow(() -> new FoodNotFoundException("해당 아이디를 가진 음식을 찾을 수 없습니다. ID: " + foodDetailReq.getId()));
+
+        List<FoodIngredientDto> ingredientDtos = food.getIngredients().stream()
+                .map(ingredient -> FoodIngredientDto.builder()
+                        .id(ingredient.getId())
+                        .name(ingredient.getName())
+                        .build())
+                .collect(Collectors.toList());
+
+        List<BlockDto> blockDtos = food.getFoodBlocks().stream()
+                .map(block -> BlockDto.builder()
+                        .id(block.getId())
+                        .content(block.getContent())
+                        .sequence(block.getSequence())
+                        .source(block.getSource())
+                        .build())
+                .collect(Collectors.toList());
+
+        FoodDetailRes foodDetailRes = FoodDetailRes.builder()
+                .id(food.getId())
+                .name(food.getName())
+                .veganType(food.getVeganType())
+                .ingredientDtos(ingredientDtos)
+                .blockDtos(blockDtos)
+                .build();
+
+        return ResponseEntity.ok(foodDetailRes);
+
+    }
 
     // 3가지 음식 랜덤 조회 : 메인 페이지
     public ResponseEntity<?> findThreeFoods() {
