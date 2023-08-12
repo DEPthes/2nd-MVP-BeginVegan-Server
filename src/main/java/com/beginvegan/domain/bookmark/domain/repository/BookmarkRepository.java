bd7dd9d9be4be936a4296d400609e1ddb962de5b
박세진 <jinpark99@naver.com>
2023-08-12 15:39:24 +0900

    feat: 식당/카페 스크랩 API 구현

--- src/main/java/com/beginvegan/domain/bookmark/domain/repository/BookmarkRepository.java
+++ src/main/java/com/beginvegan/domain/bookmark/domain/repository/BookmarkRepository.java
@@ -1,6 +1,7 @@
 package com.beginvegan.domain.bookmark.domain.repository;
 
 import com.beginvegan.domain.bookmark.domain.Bookmark;
+import com.beginvegan.domain.restaurant.domain.Restaurant;
 import com.beginvegan.domain.user.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,5 +12,7 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
     @EntityGraph(attributePaths = {"restaurant"})
     Page<Bookmark> findBookmarksByUser(User user, Pageable pageable);
+
+    boolean existsBookmarkByUserAndRestaurant(User user, Restaurant restaurant);
 
 }
--- src/main/java/com/beginvegan/domain/restaurant/presentation/RestaurantController.java
+++ src/main/java/com/beginvegan/domain/restaurant/presentation/RestaurantController.java
@@ -7,6 +7,7 @@ import com.beginvegan.domain.review.dto.ReviewListRes;
 import com.beginvegan.global.config.security.token.CurrentUser;
 import com.beginvegan.global.config.security.token.UserPrincipal;
 import com.beginvegan.global.payload.ErrorResponse;
+import com.beginvegan.global.payload.Message;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -49,6 +50,19 @@ public class RestaurantController {
             @Parameter(description = "식당/카페의 리뷰 목록을 페이지별로 조회합니다. **Page는 0부터 시작합니다!**", required = true) @RequestParam(value = "page") Integer page
     ) {
         return restaurantService.findRestaurantReviewsById(restaurantId, page);
+    }
+
+    @Operation(summary = "식당/카페 스크랩", description = "식당/카페를 스크랩합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "식당/카페 스크랩 성공", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Message.class)) } ),
+            @ApiResponse(responseCode = "400", description = "식당/카페 스크랩 실패", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class) ) } ),
+    })
+    @PostMapping("/{restaurant-id}")
+    public ResponseEntity<?> scrapRestaurant(
+            @Parameter(description = "AccessToken을 입력해주세요") @CurrentUser UserPrincipal userPrincipal,
+            @Parameter(description = "식당/카페ID로 스크랩합니다.", required = true) @PathVariable(value = "restaurant-id") Long restaurantId
+    ) {
+        return restaurantService.scrapRestaurant(userPrincipal, restaurantId);
     }
 
 }
